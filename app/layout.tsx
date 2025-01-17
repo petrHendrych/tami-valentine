1f64fabd00b947e69acd92a76c39e33c3620df01
Petr Hendrych <petr.hendrych97@seznam.cz>
2025-01-18 00:51:15 +0100

    Creating main pink page with basic layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Playwrite_VN } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const playwriteVN = Playwrite_VN({
+  variable: "--font-playwrite-mono",
 });
 
 export const metadata: Metadata = {
@@ -23,10 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={playwriteVN.className}>
+      <body className="antialiased">
         {children}
       </body>
     </html>
