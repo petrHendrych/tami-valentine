20cb4f5e39b52290798ae276d7501f3accc7c8dc
Petr Hendrych <petr.hendrych97@seznam.cz>
2025-01-19 21:55:28 +0100

    Circling sad cats when clicking no button

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,58 @@
 'use client'
 
 import Image from "next/image";
-import Buttons from "@/app/Buttons";
+import {useRef} from "react";
+import {useButtonClick} from "@/app/useButtonClick";
+import cat1 from "@/public/sad-cats/sad-cat-1.jpg";
+import cat2 from "@/public/sad-cats/sad-cat-2.jpg";
+import cat3 from "@/public/sad-cats/sad-cat-3.jpg";
+import cat4 from "@/public/sad-cats/sad-cat-4.jpg";
+import cat5 from "@/public/sad-cats/sad-cat-5.jpg";
+import cat6 from "@/public/sad-cats/sad-cat-6.jpeg";
 
 export default function Home() {
+    const noButton = useRef<HTMLButtonElement>(null);
+    const yesButton = useRef<HTMLButtonElement>(null);
+
+    const sadCats = [cat1, cat2, cat3, cat4, cat5, cat6]
+
+    const {moveButton, clickIndex} = useButtonClick(noButton, yesButton)
+
+    console.log(clickIndex)
+
     return (
         <div className="h-dvh w-full flex items-center justify-center min-h-full relative">
             <div className="flex items-center justify-center flex-col gap-10 px-5">
-                <div className="text-xl drop-shadow-lg text-center">Sorry for waiting so long. I wanted to come up with something creative 🥺, so...</div>
-                <Image src="/cat_with_flower.jpg" alt="Cat With Flower"
-                       priority
-                       fetchPriority="high"
-                       className="rounded-2xl"
-                       width={400}
-                       height={0}/>
-                <div className="drop-shadow-2xl text-xl xs:text-xl sm:text-3xl text-center leading-loose">💖 Will you be my Valentine? 💖</div>
-                <Buttons />
+                <div className="text-xl drop-shadow-lg text-center">Sorry for waiting so long. I wanted to come up with
+                    something creative 🥺, so...
+                </div>
+                {clickIndex ?
+                    (<Image src={sadCats[clickIndex - 1].src} alt="Sad cat"
+                            width={sadCats[clickIndex - 1].width}
+                            height={sadCats[clickIndex - 1].width}
+                            priority
+                            fetchPriority="high"
+                            className="rounded-2xl max-w-[400px] min-w-[300px]"/>
+                    ) : (
+                        <Image src="/cat_with_flower.jpg" alt="Cat With Flower"
+                               priority
+                               fetchPriority="high"
+                               className="rounded-2xl"
+                               width={400}
+                               height={0}/>
+                    )
+                }
+
+                <div className="drop-shadow-2xl text-xl xs:text-xl sm:text-3xl text-center leading-loose">💖 Will you be
+                    my Valentine? 💖
+                </div>
+
+                <div className="flex gap-3">
+                    <button ref={yesButton} className="bg-green-600 p-4 px-6 rounded-2xl">Yes</button>
+                    <button ref={noButton} className="bg-red-700 p-4 px-6 rounded-2xl relative transition-transform"
+                            onClick={moveButton}>No
+                    </button>
+                </div>
             </div>
         </div>
     );
--- app/useButtonClick.ts
+++ app/useButtonClick.ts
@@ -1,15 +1,15 @@
-'use client'
+import {RefObject, useState} from "react";
 
-import {useMemo, useRef, useState} from "react";
-
-const Buttons = () => {
+export const useButtonClick = (noButton: RefObject<HTMLButtonElement | null>, yesButton: RefObject<HTMLButtonElement | null>) => {
     const [scale, setScale] = useState(1)
-
-    const noButton = useRef<HTMLButtonElement>(null);
-    const yesButton = useRef<HTMLButtonElement>(null);
+    const [clickIndex, setClickIndex] = useState(0)
 
     const moveButton = () => {
         if (noButton.current && yesButton.current) {
+            setClickIndex(prevState => {
+                if (prevState + 1 > 6) return 1
+                return prevState + 1
+            })
             const randomTopOffset = Math.floor(Math.random() * window.innerHeight) - noButton.current.clientHeight;
             const randomLeftOffset = Math.floor(Math.random() * window.innerWidth) - noButton.current.clientWidth;
             noButton.current.style.position = "absolute";
@@ -18,19 +18,8 @@ const Buttons = () => {
 
             setScale(prevState => prevState + Math.random() / 10);
             yesButton.current.style.transform = `scale(${scale > 1.5 ? 1.5 : scale})`;
-            }
         }
-
-
-        return (
-            <div className="flex gap-3">
-                <button ref={yesButton} className="bg-green-600 p-4 px-6 rounded-2xl">Yes</button>
-                <button ref={noButton} className="bg-red-700 p-4 px-6 rounded-2xl relative transition-transform"
-                        onClick={moveButton}>No</button>
-            </div>
-
-    );
     }
-;
 
-export default Buttons;
+    return {moveButton, clickIndex};
+}
